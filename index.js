da61451d318f6fae361a60fd4e0502fc7606c9fd
Gaston Acosta <gasteac@gmail.com>
2024-07-17 15:04:49 -0300

    chore: Update CORS configuration for gasteac.com origin

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import { sendMessage } from "./controllers/message.js";
 mongoose.connect(process.env.DB_CNN).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
 
 const app = express();
-app.use(cors('https://gasteac.com'));
 app.use(express.json());
+app.use(cors('https://gasteac.com'));
 app.use("/api/form", sendMessage);
 app.listen(process.env.PORT, () => {
   console.log(`Running at port ${process.env.PORT}`);
