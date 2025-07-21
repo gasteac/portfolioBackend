a1d78ecfb6639eeb3610b46d7172a19e5951e182
Gaston Acosta <gaston.acosta@promtior.ai>
2025-07-21 14:41:25 -0300

    chore: Update CORS configuration to allow localhost on port 5173 for development

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ sequelize.authenticate()
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['https://gasteac.com', 'http://localhost:5174'],
+  origin: ['https://gasteac.com', 'http://localhost:5173'],
   credentials: true
 }));
 app.use("/api/form", messageRouter);
