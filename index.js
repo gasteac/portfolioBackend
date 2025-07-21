048f32042f076e3e7fbf08874ea6e8e862d4bd66
Gaston Acosta <gaston.acosta@promtior.ai>
2025-07-21 14:40:38 -0300

    chore: Remove Mongoose connection and update CORS to allow localhost for development

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ import cors from "cors";
 import messageRouter from "./routes/message.js";
 import { sequelize } from "./models/Message.js";
 
-// Eliminar la conexión de Mongoose
-// mongoose.connect(process.env.DB_CNN).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
-
-// Probar conexión a PostgreSQL
 sequelize.authenticate()
   .then(() => console.log("Connected to PostgreSQL"))
   .catch((err) => console.log("PostgreSQL connection error:", err));
@@ -17,7 +13,7 @@ sequelize.authenticate()
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['https://gasteac.com'],
+  origin: ['https://gasteac.com', 'http://localhost:5174'],
   credentials: true
 }));
 app.use("/api/form", messageRouter);
